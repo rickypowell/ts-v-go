6951c8ab75f8b3685bf15e18ad55efde1729589d
Ricky Powell <rickypowell@users.noreply.github.com>
2022-02-26 15:19:27 -0600

    added MyData type for ts

--- main.ts
+++ main.ts
@@ -3,12 +3,16 @@ function getMyData() {
     return Promise.resolve(`{ "name": "ricky", "age": 99 }`);
 }
 
+type MyData = {
+    age: number;
+};
+
 async function main() {
     try {
 
         const payload = await getMyData();
         console.log('before', payload);
-        const result = JSON.parse(payload);
+        const result: MyData = JSON.parse(payload);
 
         const { age } = result;
 
